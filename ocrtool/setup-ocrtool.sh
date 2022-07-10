dbd25d05cf854bf195300b83c011290f06e40915
Calli Brucke <calli.brucke@gmail.com>
2022-07-10 13:27:45 +0200

    automate apt a bit further with -y flag

--- ocrtool/setup-ocrtool.sh
+++ ocrtool/setup-ocrtool.sh
@@ -1,6 +1,5 @@
 sudo apt update && \
-    sudo apt upgrade && \
-    sudo apt install ocrmypdf tesseract-ocr tesseract-ocr-deu tesseract-ocr-spa tesseract-ocr-fra python3-pip wget && \
+    sudo apt install -y ocrmypdf tesseract-ocr tesseract-ocr-deu tesseract-ocr-spa tesseract-ocr-fra python3-pip wget && \
     sudo pip install ocrmypdfgui && \
     cd ~/.local/share/applications && \
     wget https://raw.githubusercontent.com/phreak1n/goebel/main/ocrtool/OCRtool.desktop
