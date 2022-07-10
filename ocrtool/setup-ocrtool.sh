0e0338476c5b8400c802f516752e4a317542f36e
Calli Brucke <calli.brucke@gmail.com>
2022-07-10 13:03:37 +0200

    add setup file for OCRtool

---
+++ ocrtool/setup-ocrtool.sh
@@ -0,0 +1,4 @@
+sudo apt install ocrmypdf tesseract-ocr tesseract-ocr-deu tesseract-ocr-spa tesseract-ocr-fra python3-pip wget && \
+sudo pip install ocrmypdfgui && \
+cd ~/.local/share/applications && \
+wget https://raw.githubusercontent.com/phreak1n/goebel/main/ocrtool/OCRtool.desktop
