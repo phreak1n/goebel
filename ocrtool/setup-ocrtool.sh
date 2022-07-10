e80ccb7c513d299bf677bd686568bab3cf09b3f4
Calli Brucke <calli.brucke@gmail.com>
2022-07-10 13:22:57 +0200

    added apt update to setup to bes sure apt is up-to-date

--- ocrtool/setup-ocrtool.sh
+++ ocrtool/setup-ocrtool.sh
@@ -1,4 +1,6 @@
-sudo apt install ocrmypdf tesseract-ocr tesseract-ocr-deu tesseract-ocr-spa tesseract-ocr-fra python3-pip wget && \
-sudo pip install ocrmypdfgui && \
-cd ~/.local/share/applications && \
-wget https://raw.githubusercontent.com/phreak1n/goebel/main/ocrtool/OCRtool.desktop
+sudo apt update && \
+    sudo apt upgrade && \
+    sudo apt install ocrmypdf tesseract-ocr tesseract-ocr-deu tesseract-ocr-spa tesseract-ocr-fra python3-pip wget && \
+    sudo pip install ocrmypdfgui && \
+    cd ~/.local/share/applications && \
+    wget https://raw.githubusercontent.com/phreak1n/goebel/main/ocrtool/OCRtool.desktop
